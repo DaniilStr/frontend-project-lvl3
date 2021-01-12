d463bdf9e201f515f850e2ad6e854bec7a2633c4
Daniil Strasser <dajka444@gmail.com>
2021-01-12 13:44:39 +0800

    first step

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ### Hexlet tests and linter status:
+
 [![Actions Status](https://github.com/DaniilStr/frontend-project-lvl3/workflows/hexlet-check/badge.svg)](https://github.com/DaniilStr/frontend-project-lvl3/actions)
+
+[![CI](https://github.com/DaniilStr/frontend-project-lvl3/workflows/CI/badge.svg)](https://github.com/DaniilStr/frontend-project-lvl3/actions)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/f5e64f9e3832e318a055/maintainability)](https://codeclimate.com/github/DaniilStr/frontend-project-lvl3/maintainability)
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+},
+  entry: {
+    main: path.resolve(__dirname, "./src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "index.bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "RSS-agregator",
+      template: "./src/template.html",
+      filename: "index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
+    ],
+  },
+};
