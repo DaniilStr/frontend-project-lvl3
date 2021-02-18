8658f8dd68a50449149f4df0dcaa350163a8ba60
Daniil Strasser <dajka444@gmail.com>
2021-02-18 18:22:33 +0800

    rename HTML files

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'RSS-agregator',
-      template: './src/template.html',
-      filename: 'index.html',
+      template: './src/index.html',
+      filename: 'index.bundle.html',
     }),
     new CleanWebpackPlugin(),
   ],
