9fe9bbf0bb0993fbd43dacc095ec8bb650f44f83
Daniil Strasser <dajka444@gmail.com>
2021-02-19 09:13:40 +0800

    rename template to index

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'RSS-agregator',
-      template: './template.html',
-      filename: 'index.html',
+      template: './index.html',
+      filename: 'index.bundle.html',
     }),
     new CleanWebpackPlugin(),
   ],
