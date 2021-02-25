64a5a148b5d70b2ed1a735cd133e5f4120f7e5ef
Daniil Strasser <dajka444@gmail.com>
2021-02-25 18:42:42 +0800

    777777777777

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     port: 8080,
   },
   entry: {
-    main: path.resolve(__dirname, './src/index.js'),
+    main: path.resolve(__dirname, './src/init.js'),
   },
   output: {
     path: path.resolve(__dirname, './dist'),
