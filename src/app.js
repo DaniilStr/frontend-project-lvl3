c3bf4ca2e5aca6be5ea683a0b45684b9f12c9b22
Daniil Strasser <dajka444@gmail.com>
2021-03-23 17:00:21 +0800

    8787878787878787878787878787878787

--- src/app.js
+++ src/app.js
@@ -95,9 +95,10 @@ export default (state, i18nextInstance) => {
     const { rssLink } = watchedState.form.fields;
 
     axios(`${proxyUrl}${encodeURIComponent(rssLink)}`, {
-      headers: { application/rss+xml; charset=utf-8 }
+      headers: { 'content-type': 'application/rss+xml; charset=utf-8' },
     })
       .then((response) => {
+        console.log('response', response);
         const feed = parse(response.data.contents);
         addFeed(feed);
         watchedState.form.fields.rssLink = '';
@@ -107,7 +108,7 @@ export default (state, i18nextInstance) => {
       .catch((err) => {
         watchedState.form.processState = 'failed';
         watchedState.networkError = err;
-        console.log('err.status from catch', err.status);
+        console.log('err from catch', err);
       });
   });
 };
